43963f954b4ae86f1bdc1cb3653ecc60eec446fb
snoozie-v <gettingsleepyz@gmail.com>
2024-12-05 21:09:13 -0600

    adding 12/5 books, added logic to only show fields that exist on books and adapted css to keep the list centered on thinner screens

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ console.log(currentYear)
 
 const data = [
     {
+        contract: "0x138Fc079CFCD214A7afF4D38533A9574D4aDF463",
+        title:"Beyond the City",
+        creation: "12/6/2024",
+        tier1_final: "1/5/2024"
+    },
+    {
+        contract: "0x6521B0e9f697c9eb8a8e33743b843bF5b8d0822c",
+        title:"Four-Fifty Miles to Freedom",
+        creation: "12/6/2024",
+        tier1_final: "1/5/2024"
+    },
+    {
+        contract: "0x290d65836FF3ccBe27E68Df92aeaE770eDa34945",
+        title:"Silas Marner",
+        creation: "12/6/2024",
+        tier3_final: "12/30/2024"
+    },
+    {
         contract: "0x7218D65DFDc072A7D93F07E20FA0b92Bf9A2Ef27",
         title:"The Coming People",
         creation: "11/26/2024",
@@ -213,15 +231,16 @@ function displayData() {
       const itemDiv = document.createElement('div');
       itemDiv.className = 'data-item'; 
       itemDiv.innerHTML = `
-        <h2><a href="https://vechainstats.com/account/${item.contract}">${item.title}</a></h2>
-        <ul>
-        <li><strong>Creation Date:</strong> ${item.creation}</li>
-          <li><strong>Tier 1 Final Day:</strong> ${item.tier1_final}</li>
-          <li><strong>Tier 2 Start Date:</strong> ${item.tier2_start}</li>
-          <li><strong>Tier 2 Final Day:</strong> ${item.tier2_final}</li>
-          <li><strong>Claiming Expire</strong> ${item.claiming_expire}</li>
-        </ul>
-      `;
+  <h2><a href="https://vechainstats.com/account/${item.contract}">${item.title}</a></h2>
+  <ul>
+    ${item.creation ? `<li><strong>Creation Date:</strong> ${item.creation}</li>` : ''}
+    ${item.tier1_final ? `<li><strong>Tier 1 Final Day:</strong> ${item.tier1_final}</li>` : ''}
+    ${item.tier2_start ? `<li><strong>Tier 2 Start Date:</strong> ${item.tier2_start}</li>` : ''}
+    ${item.tier2_final ? `<li><strong>Tier 2 Final Day:</strong> ${item.tier2_final}</li>` : ''}
+    ${item.claiming_expire ? `<li><strong>Claiming Expire:</strong> ${item.claiming_expire}</li>` : ''}
+    ${item.tier3_final ? `<li><strong>Tier 3 Final Day:</strong> ${item.tier3_final}<li>` : ''}
+  </ul>
+`;
   
       dataContainer.appendChild(itemDiv);
     });
